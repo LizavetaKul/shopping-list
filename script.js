157328fc7d3d0da05a2ba2e2c95e59119b0c6783
LizavetaKul <110459668+LizavetaKul@users.noreply.github.com>
2022-08-22 23:54:10 +0300

    Реализовала на JSc список покупок

--- script.js
+++ script.js
@@ -1,4 +1,22 @@
 /* Новые элементы должны добавляться в список по нажатию на Enter */
+const input = document.querySelector('#input');
+const items = document.querySelector('.items');
+
+input.addEventListener('keydown', function(event) {
+    if (event.key == 'Enter') {
+        const listText = input.value;
+        const newMessage = document.createElement('div');
+        newMessage.classList.add('item')
+        newMessage.textContent = listText;
+        items.append(newMessage);
+        input.value = '';
+
+
+        newMessage.addEventListener('click', () => {
+            newMessage.classList.toggle('done');
+        })
+    }
+})
 
 /* Пустые элементы не должны добавляться */
 
